b97310ee88ccc150c144efd2ba5db7eb6e3f0d3a
OpenShift guest <nihlanth@gmail.com>
2013-10-06 23:39:08 -0400

    improved styling

--- listify.js
+++ listify.js
@@ -2,20 +2,14 @@ $(document).ready(function() {
   
   $('.new-item').keydown(function(event){
     var submit = event.which;
-    //document.write(submit);
     if (submit == 13) {
-   
     var text = $('#new-filter-string').val();
-      //if(text) {
-        //document.write(text);
+      if(text) {
 	    $('.filter-string-list').append("<div class='addingitem'><div class='smallbox'></div>" + text + "</div>");
-   //   }
-   
+      }
     }
-    //return false;
-    
   });
-  $('.filter-string-list').click('li', function(ev) {
+  $('.filter-string-list').click(function(ev) {
     $(ev.target).remove();
   });
 
