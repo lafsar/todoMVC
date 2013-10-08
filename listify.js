80e78d5087a3e5ff9cc399708eadc95d7aa6eeab
OpenShift guest <nihlanth@gmail.com>
2013-10-07 23:46:44 -0400

    more attempts to make checkbox work

--- listify.js
+++ listify.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
-  
   $('.new-item').keydown(function(event){
     var submit = event.which;
     if (submit == 13) {
     var text = $('#new-filter-string').val();
       if(text) {
-	    $('.filter-string-list').append("<div class='addingitem'><div class='smallbox'></div>" + text + "</div>");
+	    $('.filter-string-list').append("<div class='addingitem'><div class='smallbox'><input type='checkbox' id='toggle-new'></div>" + text + "</div>");
       }
     }
   });
-  $('.filter-string-list').click(function(ev) {
-    $(ev.target).remove();
+  
+ $('#toggle-new').change(function(ev) {
+   console.log('hello');
+   var target = $(ev.target);
+   console.log(target.is(':checked'));
+    if (target.prop('checked'))
+    {
+      
+    }
+
+ //   $(ev.target).css('text-decoration', 'line-through');
   });
+  
 
 });
