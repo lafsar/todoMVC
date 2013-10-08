e93185806da2288c2e8eb30b3c40c7cf23fe13f0
OpenShift guest <nihlanth@gmail.com>
2013-10-08 03:57:28 -0400

    implemented checkbox crossout functionality

--- listify.js
+++ listify.js
@@ -1,25 +1,43 @@
 $(document).ready(function() {
-  $('.new-item').keydown(function(event){
+  $('.new-item').keydown(function(event) {
     var submit = event.which;
-    if (submit == 13) {
+    if (submit == 13)
+    {
     var text = $('#new-filter-string').val();
-      if(text) {
-	    $('.filter-string-list').append("<div class='addingitem'><div class='smallbox'><input type='checkbox' id='toggle-new'></div>" + text + "</div>");
+      if(text)
+      {
+	    $('.filter-string-list')
+	    .append("<div class='addingitem'><div class='smallbox'><input type='checkbox' class='toggle-new'></div>" + text + "</div>");
+       $('.toggle').change(function(ev) {
+         var target = $(ev.target);
+         if ($(target).prop('checked'))
+         {
+         $('.addingitem')
+         .css('text-decoration', 'line-through');
+         $('.toggle-new')
+         .prop('checked', true);
+         }
+         else
+         {
+         $('.addingitem')
+         .css('text-decoration', 'none');
+         $('.toggle-new')
+         .prop('checked', false);
+         }
+       });
+       $('.toggle-new').change(function(ev) {
+        var target = $(ev.target);
+        if ($(target).prop('checked'))
+        {
+          $(this)
+          .parents('.addingitem')
+          .css('text-decoration', 'line-through');
+        } else
+          $(this)
+          .parents('.addingitem')
+          .css('text-decoration', 'none');
+       });
       }
     }
   });
-  
- $('#toggle-new').change(function(ev) {
-   console.log('hello');
-   var target = $(ev.target);
-   console.log(target.is(':checked'));
-    if (target.prop('checked'))
-    {
-      
-    }
-
- //   $(ev.target).css('text-decoration', 'line-through');
-  });
-  
-
 });
