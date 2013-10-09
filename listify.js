0a3165324f59df0f4a68c0ae6d74648e692b8c47
OpenShift guest <nihlanth@gmail.com>
2013-10-08 22:56:14 -0400

    got the item count working

--- listify.js
+++ listify.js
@@ -3,41 +3,54 @@ $(document).ready(function() {
     var submit = event.which;
     if (submit == 13)
     {
-    var text = $('#new-filter-string').val();
+      var text = $('#new-filter-string').val();
       if(text)
       {
-	    $('.filter-string-list')
-	    .append("<div class='addingitem'><div class='smallbox'><input type='checkbox' class='toggle-new'></div>" + text + "</div>");
-       $('.toggle').change(function(ev) {
-         var target = $(ev.target);
-         if ($(target).prop('checked'))
-         {
-         $('.addingitem')
-         .css('text-decoration', 'line-through');
-         $('.toggle-new')
-         .prop('checked', true);
-         }
-         else
-         {
-         $('.addingitem')
-         .css('text-decoration', 'none');
-         $('.toggle-new')
-         .prop('checked', false);
-         }
-       });
-       $('.toggle-new').change(function(ev) {
-        var target = $(ev.target);
-        if ($(target).prop('checked'))
-        {
-          $(this)
-          .parents('.addingitem')
-          .css('text-decoration', 'line-through');
-        } else
-          $(this)
-          .parents('.addingitem')
-          .css('text-decoration', 'none');
-       });
+	      $('.filter-string-list')
+	      .append("<div class='addingitem'><div class='smallbox'><input type='checkbox' class='toggle-new'></div>" + text + "</div>");
+	      var len = $('.addingitem').length;
+        $('.toggle').change(function(ev) {
+          var target = $(ev.target);
+          if ($(target).prop('checked'))
+          {
+            $('.addingitem')
+            .css('text-decoration', 'line-through');
+            $('.toggle-new')
+            .prop('checked', true);
+            var newd = 0;
+          $('#todo-count').html('<strong>'+ newd +'</strong> items left');
+          }
+          else
+          {
+            $('.addingitem')
+            .css('text-decoration', 'none');
+            $('.toggle-new')
+            .prop('checked', false);
+            $('#todo-count').html('<strong>'+ len +'</strong> items left');
+          }
+        });
+        $('.toggle-new').change(function(ev) {
+          var target = $(ev.target);
+          if ($(target).prop('checked'))
+          {
+            $(this)
+            .parents('.addingitem')
+            .css('text-decoration', 'line-through');
+            len--;
+            $('#todo-count').html('<strong>' + len +'</strong> items left');
+          } 
+          else
+          {
+            $(this)
+            .parents('.addingitem')
+            .css('text-decoration', 'none');
+            len++;
+            $('#todo-count').html('<strong>'+ len +'</strong> items left');
+          }
+        });
       }
+      $('#todo-count').html('<strong>'+ len +'</strong> items left');
     }
   });
 });
+
